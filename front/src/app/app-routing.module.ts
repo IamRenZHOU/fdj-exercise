2bfd938f16d9b28c70c74dbbec047f612f4febfb
vboxuser <vboxuser@debian.myguest.virtualbox.org>
2023-01-10 22:52:52 +0100

    Setup front with route and main components

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, ActivatedRoute, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+import { PlayerComponent } from './player/player.component';
+import { TeamListComponent } from './team-list/team-list.component';
+import { TeamComponent } from './team/team.component';
+
+const routes: Routes = [
+  { path: '',   redirectTo: '/teams', pathMatch: 'full' },
+  { path: 'teams', component: TeamListComponent },
+  { path: 'team/:id', component: TeamComponent },
+  { path: 'player/:id', component: PlayerComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlayerComponent } from './player/player.component';
+import { TeamListComponent } from './team-list/team-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayerComponent,
+    TeamListComponent
   ],
   imports: [
     BrowserModule,
