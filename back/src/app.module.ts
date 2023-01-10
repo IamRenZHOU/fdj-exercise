cc334b04f8f95cba6b730354cd83afcb7b8321d4
Ren ZHOU <zhou_ren.fr@hotmail.com>
2023-01-10 23:37:06 +0100

    Add service and controller for team

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { TeamController } from './team/team.controller'
+import { TeamService } from './team/team.service'
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, TeamController],
+  providers: [AppService, TeamService],
 })
 export class AppModule {}
